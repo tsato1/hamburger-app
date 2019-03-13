3b509587dfe56c21a577e012d58448818c666ad8
tsato1 <a8tb1117@gmail.com>
2019-03-12 19:43:57 -0700

    159: more/less buttom and its functionality

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styles from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl.js'
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => (
+    <div className={styles.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientsAdded(ctrl.type)}
+                removed={() => props.ingredientsRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
+        ))}
+    </div>
+);
+
+export default buildControls
