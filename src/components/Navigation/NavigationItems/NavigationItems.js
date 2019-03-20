dc406599ed0e9e7c921fc27b5a33f0c1d71c9783
tsato1 <a8tb1117@gmail.com>
2019-03-19 23:21:31 -0700

    172 sidebar added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './NavigationItems.module.css'
 import NavigationItem from './NavigationItem/NavigationItem'
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={styles.NavigationItems}>
         <NavigationItem link="/" active>Burger Builder</NavigationItem>
         <NavigationItem link="/">Checkout</NavigationItem>
